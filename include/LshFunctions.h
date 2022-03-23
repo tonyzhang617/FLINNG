657c53ea2ca61420a3c99a724ddcb7e131751347
Tianyi Zhang <tonyzhang617@gmail.com>
2022-03-23 12:29:47 -0500

    Extract implementation from header for LshFunctions

--- include/LshFunctions.h
+++ include/LshFunctions.h
@@ -7,174 +7,33 @@
 #include <iostream>
 #include <vector>
 
-uint64_t combine(uint64_t item1, uint64_t item2) {
-  return item1 * 0xC4DD05BF + item2 * 0x6C8702C9;
-}
+uint64_t combine(uint64_t item1, uint64_t item2);
 
 void single_densified_minhash(uint64_t *result, const uint64_t *point,
                               uint64_t point_len, uint64_t num_tables,
                               uint64_t hashes_per_table, uint8_t hash_range_pow,
-                              uint32_t random_seed) {
-
-  uint64_t num_hashes_to_generate = num_tables * hashes_per_table;
-  std::vector<uint64_t> prelim_result(num_hashes_to_generate);
-  uint64_t binsize = std::ceil(UINT64_MAX / prelim_result.size());
-
-  for (uint64_t i = 0; i < num_hashes_to_generate; i++) {
-    prelim_result[i] = UINT64_MAX;
-  }
-
-  for (uint64_t i = 0; i < point_len; i++) {
-    uint64_t val = point[i];
-    val *= random_seed;
-    val ^= val >> 13;
-    val *= 0x192AF017AAFFF017;
-    val *= val;
-    uint64_t hash = val;
-    uint64_t binid =
-        std::min((uint64_t)floor(val / binsize), num_hashes_to_generate - 1);
-    if (prelim_result[binid] > hash) {
-      prelim_result[binid] = hash;
-    }
-  }
-
-  // Densify
-  for (size_t i = 0; i < num_hashes_to_generate; i++) {
-    uint64_t next = prelim_result[i];
-    if (next != UINT64_MAX) {
-      continue;
-    }
-    uint64_t count = 0;
-    while (next == UINT64_MAX) {
-      count++;
-      uint64_t index = combine(i, count) % num_hashes_to_generate;
-      next = prelim_result[index]; // Kills GPU.
-      if (count > 100) {           // Densification failure.
-        next = 0;
-        break;
-      }
-    }
-    prelim_result[i] = next;
-  }
-
-  // Combine each K
-  for (uint64_t table = 0; table < num_tables; table++) {
-    result[table] = prelim_result[hashes_per_table * table];
-    for (uint64_t hash = 1; hash < hashes_per_table; hash++) {
-      result[table] =
-          combine(prelim_result[hashes_per_table * table], result[table]);
-    }
-    result[table] >>= (64 - hash_range_pow);
-  }
-}
+                              uint32_t random_seed);
 
 std::vector<uint64_t>
 parallel_densified_minhash(const uint64_t *points, uint64_t num_points,
                            uint64_t point_dimension, uint64_t num_tables,
                            uint64_t hashes_per_table, uint8_t hash_range_pow,
-                           uint32_t random_seed) {
-
-  std::vector<uint64_t> result(num_tables * num_points);
-
-#pragma omp parallel for
-  for (uint64_t point_id = 0; point_id < num_points; point_id += 1) {
-    single_densified_minhash((&result[0]) + point_id * num_tables,
-                             points + point_id * point_dimension,
-                             point_dimension, num_tables, hashes_per_table,
-                             hash_range_pow, random_seed);
-  }
-
-  return result;
-}
+                           uint32_t random_seed);
 
 std::vector<uint64_t>
 parallel_densified_minhash(const std::vector<std::vector<uint64_t>> &points,
                            uint64_t num_tables, uint64_t hashes_per_table,
-                           uint8_t hash_range_pow, uint32_t random_seed) {
-
-  std::vector<uint64_t> result(num_tables * points.size());
-
-#pragma omp parallel for
-  for (uint64_t point_id = 0; point_id < points.size(); point_id += 1) {
-    single_densified_minhash((&result[0]) + point_id * num_tables,
-                             (&points[point_id][0]), points[point_id].size(),
-                             num_tables, hashes_per_table, hash_range_pow,
-                             random_seed);
-  }
-
-  return result;
-}
+                           uint8_t hash_range_pow, uint32_t random_seed);
 
 std::vector<uint64_t> parallel_srp(const float *dense_data, uint64_t num_points,
                                    uint64_t data_dimension, int8_t *random_bits,
                                    uint64_t num_tables,
-                                   uint64_t hashes_per_table) {
-  std::vector<uint64_t> result(num_tables * num_points);
-
-#pragma omp parallel for
-  for (uint64_t data_id = 0; data_id < num_points; data_id++) {
-    for (uint64_t rep = 0; rep < num_tables; rep++) {
-      uint64_t hash = 0;
-      for (uint64_t bit = 0; bit < hashes_per_table; bit++) {
-        double sum = 0;
-        for (uint64_t j = 0; j < data_dimension; j++) {
-          double val = dense_data[data_dimension * data_id + j];
-          if (random_bits[rep * hashes_per_table * data_dimension +
-                          bit * data_dimension + j] > 0) {
-            sum += val;
-          } else {
-            sum -= val;
-          }
-        }
-        hash += (sum > 0) << bit;
-      }
-      result[data_id * num_tables + rep] = hash;
-    }
-  }
-
-  return result;
-}
+                                   uint64_t hashes_per_table);
 
 std::vector<uint64_t> parallel_l2_lsh(const float *dense_data, uint64_t num_points,
                                       uint64_t data_dimension, int8_t *random_bits,
                                       uint64_t num_tables,
                                       uint64_t hashes_per_table,
                                       uint64_t sub_hash_bits = 2,
-                                      uint64_t cutoff = 6) {
-  std::vector<uint64_t> result(num_tables * num_points);
-  uint64_t bin_width = 2 * cutoff / (1 << sub_hash_bits);
-  uint64_t num_bins = cutoff / bin_width * 2;
-  double db_bin_width = static_cast<double>(bin_width);
-
-#pragma omp parallel for
-  for (uint64_t data_id = 0; data_id < num_points; data_id++) {
-    for (uint64_t rep = 0; rep < num_tables; rep++) {
-      uint64_t hash = 0;
-      for (uint64_t bit = 0; bit < hashes_per_table; bit++) {
-        double sum = 0;
-        for (uint64_t j = 0; j < data_dimension; j++) {
-          double val = dense_data[data_dimension * data_id + j];
-          if (random_bits[rep * hashes_per_table * data_dimension +
-                          bit * data_dimension + j] > 0) {
-            sum += val;
-          } else {
-            sum -= val;
-          }
-        }
-        sum = floor(sum / db_bin_width);
-        int64_t sub_hash = static_cast<int64_t>(sum) + num_bins / 2;
-        if (sub_hash < 0) {
-          sub_hash = 0;
-        } else if (sub_hash >= static_cast<int64_t>(num_bins)) {
-          sub_hash = num_bins - 1;
-        }
-        hash += static_cast<uint64_t>(sub_hash) << (bit * sub_hash_bits);
-      }
-      result[data_id * num_tables + rep] = hash;
-    }
-  }
-
-  return result;
-}
-
+                                      uint64_t cutoff = 6);
 #endif
